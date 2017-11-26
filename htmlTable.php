38018fe08c5917149c722ad335357f481ea72ff2
Srujana Manigonda <sm2555@afsconnect1.njit.edu>
2017-11-26 13:35:54 -0500

    eleven

--- htmlTable.php
+++ htmlTable.php
@@ -3,7 +3,9 @@ class htmlTable
 {
     static function createTable($data)
     {
-        echo '<table>';
+        echo '<table border=1>';
+        echo '<tr><th>id</th><th>email</th><th>fname</th><th>lname</th><th>phone</th><th>birthday</th><th>gender</th><th>password</th></tr>';
+
         foreach ($data as $row)
         {
             echo "<tr>";
