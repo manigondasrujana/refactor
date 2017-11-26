3a399d4bc3aabe822963786a123653f42d91e643
Srujana Manigonda <sm2555@afsconnect1.njit.edu>
2017-11-25 20:31:44 -0500

    account

---
+++ account.php
@@ -0,0 +1,20 @@
+<?php
+interface model
+{
+public fuction save();
+public function delete();
+}
+final class account extends activerecord\model {
+ public $id;
+ public $email;
+ public $fname;
+ public $lname;
+ public $phone;
+ public $birthday;
+ public $gender;
+ public $password;
+ public static function tname(){
+        $tableName='accounts';
+        return $tableName;
+   }
+?>
