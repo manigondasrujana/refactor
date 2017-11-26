37c48bf82af9d8fa74284dcfbbec16a556383817
Srujana Manigonda <sm2555@afsconnect1.njit.edu>
2017-11-25 20:59:14 -0500

    index

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
+
+define('DATABASE', 'sm2555');
+define('USERNAME', 'sm2555');
+define('PASSWORD', 'tzw8bjLL');
+define('CONNECTION', 'sql1.njit.edu');
+include 'account.php';
+include 'accounts.php';
+include 'collection.php';
+include "connection.php";
+include "model.php";
+include "table.php";
+include 'todo.php';
+include "todos.php";
+?>
