d34128876753e2ce5a7f716bcb4d59eb549cc9a8
Srujana Manigonda <sm2555@afsconnect1.njit.edu>
2017-11-26 13:30:15 -0500

    five

--- collection.php
+++ collection.php
@@ -1,32 +1,33 @@
 <?php
 namespace activerecord;
-
 abstract class collection {
-    static public function create() {
+
+public static function create() {
     $model = new static::$modelName;
     return $model;
+    
     }
-    static public function findAll() {
+    public static function findAll() {
     $db = dbConn::getConnection();
     $tableName = get_called_class();
     $sql = 'SELECT * FROM ' . $tableName;
     $statement = $db->prepare($sql);
     $statement->execute();
     $class = static::$modelName;
-    $statement->setFetchMode(PDO::FETCH_CLASS, $class);
+    $statement->setFetchMode(\PDO::FETCH_CLASS, $class);
     $recordsSet =  $statement->fetchAll();
     return $recordsSet;
     }
-	static public function findOne($id) {
+	public static function findOne($id) {
         $db = dbConn::getConnection();
 	$tableName = get_called_class();
 	$sql = 'SELECT * FROM ' . $tableName . ' WHERE id =' . $id;
 	$statement = $db->prepare($sql);
 	$statement->execute();
 	$class = static::$modelName;
-	$statement->setFetchMode(PDO::FETCH_CLASS, $class);
+	$statement->setFetchMode(\PDO::FETCH_CLASS, $class);
 	$recordsSet =  $statement->fetchAll();
-	return $recordsSet[0];
+	return $recordsSet;
 	}
 	}
  ?>
