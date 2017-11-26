e706b596b3cf901384dc358315641b5d78534d39
Srujana Manigonda <sm2555@afsconnect1.njit.edu>
2017-11-26 13:31:11 -0500

    eight

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+
 ini_set('display_errors', 'On');
 error_reporting(E_ALL);
 
@@ -6,12 +7,20 @@ define('DATABASE', 'sm2555');
 define('USERNAME', 'sm2555');
 define('PASSWORD', 'tzw8bjLL');
 define('CONNECTION', 'sql1.njit.edu');
-include 'account.php';
-include 'accounts.php';
+
+include 'model.php';
+include 'database.php';
 include 'collection.php';
-include "connection.php";
-include "model.php";
-include "table.php";
-include 'todo.php';
+include "account.php";
 include "todos.php";
+include "todo.php";
+include 'accounts.php';
+include "htmlTable.php";
+
+echo "<h1><u> This is PHP ActiveRecord Assignment </u></h1>";
+$obj = new Account;
+$obj->save();
+$obj =  accounts::create();
+$result = $obj -> findAll();
+htmlTable::createTable($result);
 ?>
