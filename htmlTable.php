325349583d81b557ec08c706054b8103ebff336d
Srujana Manigonda <sm2555@afsconnect1.njit.edu>
2017-11-27 13:17:50 -0500

    final

--- htmlTable.php
+++ htmlTable.php
@@ -16,5 +16,21 @@ class htmlTable
         }
         echo "</table>";
     }
+  static function createTable1($data)
+    {
+        echo '<table border=1>';
+                    echo '<tr><th>ID</th><th>OwnerEmail</th><th>OwnerId</th><th>createddate</th><th>duedate</th><th>message</th><th>isdone</th></tr>';
+
+
+        foreach ($data as $row)
+        {
+            echo "<tr>";
+            foreach ($row as $column) {
+                echo "<td>$column</td>";
+            }
+            echo "</tr>";
+        }
+        echo "</table>";
+    }
 }
 ?>:
